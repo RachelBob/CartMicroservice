5127f91cb76c28a69b9fa97a05ff4731957b1b07
RachelBob <bobbyrachel@gmail.com>
2022-10-23 13:45:21 +0530

    Feign client changes

--- src/main/java/com/lti/cartitemservice/consumer/CustomerDetailsConsumer.java
+++ src/main/java/com/lti/cartitemservice/consumer/CustomerDetailsConsumer.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="customer-microservice-ecommerce", url="http://localhost:8081/customer-microservice-ecommerce")
+@FeignClient(name="${feign.client.name}", url="${feign.client.microservice.endpoint}")
 public interface CustomerDetailsConsumer {
 
-	@GetMapping("/customers/uuid/{uuid}")
+	@GetMapping("/uuid/{uuid}")
 	public String getCustomerByUUID(@PathVariable String uuid);
 }
