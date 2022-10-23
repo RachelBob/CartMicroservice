1ca8108b78b76c479b9a6da66fa99b1511efe9a1
RachelBob <bobbyrachel@gmail.com>
2022-10-23 12:19:49 +0530

    minor property changes

--- src/main/java/com/lti/cartitemservice/consumer/CustomerDetailsConsumer.java
+++ src/main/java/com/lti/cartitemservice/consumer/CustomerDetailsConsumer.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="CUSTOMER-MICROSERVICE")
+@FeignClient(name="customer-microservice-ecommerce", url="http://localhost:8081/customer-microservice-ecommerce")
 public interface CustomerDetailsConsumer {
 
 	@GetMapping("/customers/uuid/{uuid}")
